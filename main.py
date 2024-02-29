e1fcdf75c5d3625851a10b1c096a29bcd2610be5
Carlos Eduardo <carloseduardoifpi@gmail.com>
2024-02-29 19:03:30 -0300

    chore: make different lessons at the same time

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from utils import create_driver, make_lesson
+import json
+from utils import (
+    create_driver,
+    make_lesson,
+    login_on_account,
+    get_username_logged_in,
+)
 from dotenv import load_dotenv
 from os import getenv
 
 
 # loading the environment variables
 load_dotenv()
-LESSON_URL = getenv("LESSON_URL")
+LESSON_URLS = json.loads(getenv("LESSON_URLS"))
 PROFILE_PATH = getenv("PROFILE_PATH")
-
-print(f"Lesson URL: {LESSON_URL}")
-print(f"Profile Path: {PROFILE_PATH}")
+SLEEP_TIME = float(getenv("SLEEP_TIME"))
+NUMBER_OF_TABS = int(getenv("NUMBER_OF_TABS"))
 
 # creating the web driver
 driver = create_driver(PROFILE_PATH)
 
+login_on_account(driver)
+
+username = get_username_logged_in(driver)
+
+print(f"Logged in as: {username}")
+wait = input("Press enter to continue")
+
 # making the lesson
-make_lesson(driver, LESSON_URL, 30)
+make_lesson(driver, LESSON_URLS, tabs=NUMBER_OF_TABS, sleep_time=SLEEP_TIME)
 
 print("Lesson finished")
 
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
+from random import randint
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+SLEEP_TIME = 1
 
 
 def create_driver(profile_path: str):
@@ -12,6 +15,39 @@ def create_driver(profile_path: str):
     options.add_argument("--disable-web-security")
 
     return webdriver.Chrome(options=options)
+
+
+def check_if_logged_in(driver: webdriver.Chrome) -> bool:
+    all_a_tags = driver.find_elements(by=By.TAG_NAME, value="a")
+
+    for a_tag in all_a_tags:
+        if a_tag.get_attribute("data-test") == "profile-tab":
+            return True
+
+    return False
+
+
+def get_username_logged_in(driver: webdriver.Chrome) -> str:
+    if not check_if_logged_in(driver):
+        return "User not logged in"
+
+    all_a_tags = driver.find_elements(by=By.TAG_NAME, value="a")
+
+    for a_tag in all_a_tags:
+        if a_tag.get_attribute("data-test") == "profile-tab":
+            return a_tag.get_attribute("href").split("/")[-1]
+
+    return "User not logged in"
+
+
+def login_on_account(driver: webdriver.Chrome):
+    try:
+        driver.get("https://www.duolingo.com/learn")
+
+        while not check_if_logged_in(driver):
+            sleep(2)
+    except Exception as e:
+        print(e)
 
 
 def press_enter(driver: webdriver.Chrome):
@@ -82,55 +118,62 @@ def get_clickable_buttons(driver: webdriver.Chrome):
     return clickable_buttons
 
 
-def make_lesson(driver: webdriver.Chrome, lesson_url: str, tabs: int):
-    print(f"Starting {tabs} tabs")
-
-    for i in range(0, tabs):
-        driver.execute_script(f"window.open('{lesson_url}')")
-        sleep(2)
-
-    print("All tabs opened\nStarting lessons")
-
-    driver.switch_to.window(driver.window_handles[0])
+def finish_lesson(driver: webdriver.Chrome):
+    sleep(SLEEP_TIME)
+    click_all_buttons(driver)
+    sleep(SLEEP_TIME)
+    press_enter(driver)
     driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+
+
+def get_random_lesson(lesson_urls: list) -> str:
+    number_of_lessons = len(lesson_urls)
+    random_lesson = lesson_urls[randint(0, number_of_lessons - 1)]
+    return random_lesson
+
+
+def start_new_lesson(driver: webdriver.Chrome, lesson_urls: str):
+    lesson_url = get_random_lesson(lesson_urls)
+    driver.execute_script(f"window.open('{lesson_url}')")
+    sleep(SLEEP_TIME)
+
+
+def make_lesson(
+    driver: webdriver.Chrome,
+    lesson_urls: list,
+    tabs: int,
+    sleep_time: float,
+):
+    global SLEEP_TIME
+    SLEEP_TIME = sleep_time
+
+    print(f"Starting {tabs} tabs")
 
     current_tab = 0
 
-    tabs_finished = set()
-
     try:
         while True:
-            if len(tabs_finished) == tabs:
-                break
+
+            while len(driver.window_handles) <= tabs:
+                start_new_lesson(driver, lesson_urls)
 
             current_tab += 1
-            if current_tab == tabs:
-                current_tab = 0
+
+            if current_tab == tabs + 1:
+                current_tab = 1
 
             driver.switch_to.window(driver.window_handles[current_tab])
 
-            if current_tab in tabs_finished:
-                continue
-
             if is_challenge_step(driver):
-                sleep(5)
+                sleep(SLEEP_TIME)
                 make_challenge_step(driver)
-                sleep(1)
-                press_enter(driver)
-                tabs_finished.add(current_tab)
-                print(f"Tab {current_tab} finished")
+                sleep(SLEEP_TIME)
+                finish_lesson(driver)
                 continue
 
             click_all_buttons(driver)
             press_enter(driver)
-
-        for i in range(0, tabs):
-            driver.switch_to.window(driver.window_handles[i])
-            click_all_buttons(driver)
-            sleep(2)
-            driver.close()
-
-        print("All tabs finished")
 
     except Exception as e:
         print(e)
